478c23fc4f3789efe2f2c470ed30cc7673ddca55
Connor Horman <chorman64@gmail.com>
2026-01-14 14:51:04 -0500

    Add Windows Support

---
+++ src/imp/windows.rs
@@ -0,0 +1,130 @@
+use core::mem;
+
+use alloc::{
+    string::{String, ToString},
+    vec::Vec,
+};
+
+use crate::Uname;
+
+use windows::Win32::{
+    Foundation::ERROR_MORE_DATA,
+    System::{
+        self,
+        SystemInformation::{
+            OSVERSIONINFOEXW, PROCESSOR_ARCHITECTURE_ALPHA, PROCESSOR_ARCHITECTURE_ALPHA64,
+            PROCESSOR_ARCHITECTURE_AMD64, PROCESSOR_ARCHITECTURE_ARM, PROCESSOR_ARCHITECTURE_ARM64,
+            PROCESSOR_ARCHITECTURE_INTEL,
+        },
+    },
+};
+
+pub fn populate_uname(x: &mut Uname) -> Result<(), ()> {
+    let mut vstr = Vec::with_capacity(System::WindowsProgramming::MAX_COMPUTERNAME_LENGTH as usize);
+
+    let mut len = vstr.capacity() as u32;
+    while let Err(e) = unsafe {
+        System::SystemInformation::GetComputerNameExW(
+            System::SystemInformation::ComputerNameNetBIOS,
+            Some(windows::core::PWSTR::from_raw(vstr.as_mut_ptr())),
+            &mut len,
+        )
+    } {
+        if e == ERROR_MORE_DATA.into() {
+            vstr.reserve(len as usize);
+        } else {
+            return Err(());
+        }
+    }
+
+    unsafe {
+        vstr.set_len(len as usize - 1);
+    }
+
+    super::populate_os_name(x);
+    x.kernel_name = "Windows NT".to_string();
+
+    x.nodename = String::from_utf16_lossy(&vstr);
+
+    let mut sysinfo = unsafe { mem::zeroed() };
+
+    unsafe { System::SystemInformation::GetSystemInfo(&mut sysinfo) };
+
+    let mut osinfo: OSVERSIONINFOEXW = unsafe { mem::zeroed() };
+    osinfo.dwOSVersionInfoSize = core::mem::size_of::<OSVERSIONINFOEXW>() as u32;
+
+    if let Err(_e) = unsafe { System::SystemInformation::GetVersionExW((&raw mut osinfo).cast()) } {
+        return Err(());
+    }
+
+    let mach = match unsafe { sysinfo.Anonymous.Anonymous.wProcessorArchitecture } {
+        PROCESSOR_ARCHITECTURE_AMD64 => "x86_64",
+        PROCESSOR_ARCHITECTURE_ARM => "arm",
+        PROCESSOR_ARCHITECTURE_ARM64 => "aarch64",
+        PROCESSOR_ARCHITECTURE_ALPHA => "alpha",
+        PROCESSOR_ARCHITECTURE_ALPHA64 => "alpha64",
+        PROCESSOR_ARCHITECTURE_INTEL => match sysinfo.wProcessorLevel {
+            ..3 => unreachable!("Surely"),
+            3 => "i386",
+            4 => "i486",
+            5 => "i586",
+            6.. => "i686",
+        },
+        _ => "**UNKNOWN ARCHITECTURE**",
+    };
+
+    x.machine = mach.to_string();
+
+    super::populate_hardware_platform(x);
+    super::populate_processor(x);
+
+    x.kernel_release = match (
+        osinfo.dwMajorVersion,
+        osinfo.dwMinorVersion,
+        osinfo.dwBuildNumber,
+        osinfo.wProductType,
+    ) {
+        (5, 0, _, _) => "Windows 2000".to_string(),
+        (5, 1, _, _) => "Windows XP".to_string(),
+        (5, 2, _, 1) => "Windows XP Professional".to_string(),
+        (5, 2, _, _) if (osinfo.wSuiteMask & 0x00008000) != 0 => "Windows Home Server".to_string(),
+        (5, 2, _, _) => "Windows Server 2003".to_string(),
+        (6, 0, _, 1) => "Windows Vista".to_string(),
+        (6, 0, _, _) => "Windows Server 2008".to_string(),
+        (6, 1, _, 1) => "Windows 7".to_string(),
+        (6, 1, _, _) => "Windows Server 2008 R2".to_string(),
+        (6, 2, _, 1) => "Windows 8".to_string(),
+        (6, 2, _, _) => "Windows Server 2012".to_string(),
+        (6, 3, _, 1) => "Windows 8.1".to_string(),
+        (6, 3, _, _) => "Windows Server 2012 R2".to_string(),
+        (10, 0, ..22000, 1) => "Windows 10".to_string(),
+        (10, 0, _, 1) => "Windows 11".to_string(),
+        (10, 0, ..17763, _) => "Windows Server 2016".to_string(),
+        (10, 0, ..20348, _) => "Windows Server 2019".to_string(),
+        (10, 0, ..26100, _) => "Windows Server 2022".to_string(),
+        (10, 0, _, _) => "Windows Server 2025".to_string(),
+        (major, minor, _, _) => alloc::format!("Unknown Kernel Release {major}.{minor}"),
+    };
+
+    let sp_end = osinfo.szCSDVersion.iter().take_while(|&&v| v != 0).count();
+    let sp = String::from_utf16_lossy(&osinfo.szCSDVersion[..sp_end]);
+
+    if !sp.is_empty() {
+        x.kernel_version = alloc::format!(
+            "NT {}.{} (Build {}) {}",
+            osinfo.dwMajorVersion,
+            osinfo.dwMinorVersion,
+            osinfo.dwBuildNumber,
+            sp
+        );
+    } else {
+        x.kernel_version = alloc::format!(
+            "NT {}.{} (Build {})",
+            osinfo.dwMajorVersion,
+            osinfo.dwMinorVersion,
+            osinfo.dwBuildNumber
+        )
+    }
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 #![no_std]
+#![deny(missing_docs)]
+
+//!
+//!
 
 extern crate alloc;
 
@@ -6,20 +10,41 @@ use alloc::string::String;
 
 mod helper;
 
+/// Contains Information about the current system
 #[derive(Clone, Debug, Hash, PartialEq, Eq, Default)]
 #[non_exhaustive]
 pub struct Uname {
+    /// The Operating System Name.
     pub sysname: String,
+    /// The Kernel Name
     pub kernel_name: String,
+    /// The Node name or local system name
     pub nodename: String,
+    /// The release version of the kernel
     pub kernel_release: String,
+    /// The specific build version of the kernel
     pub kernel_version: String,
+    /// The current Host Machine Architecture
     pub machine: String,
+    /// The Processor Name
+    /// If known, this corresponds to a specific string that can be used with `-march`-like and `-mtune`-like flags in compilers to correspond closely with both feature support and timing information.
+    /// Otherwise, it is the same as [`Uname::machine`]
     pub processor: String,
+    /// The Hardware Platform.
+    ///
+    /// This is the specific version of the [`Uname::machine`]. If not known for a given architecture, it is set to be the same as [`Uname::machine`]
+    /// On x86_64, this corresponds to the microarchitecture level supported (according to cpuid).
+    /// On x86-32 this is one of i386, i486, i586, i686, or i786 (depending on the cpu implemented).
+    /// Note that the default implementation for x86-32 (used other than on windows or lilium) will never report i386,
+    /// as the default implementation requires the processor to support the cpuid instruction
+    ///
+    ///
+    /// This may be extended to support other architectures in the future
     pub hardware_platform: String,
 }
 
 impl Uname {
+    /// Constructs a new, blank [`Uname`] instance, with all fields set to empty strings.
     pub const fn new() -> Uname {
         Self {
             sysname: String::new(),
@@ -66,11 +91,15 @@ mod imp {
         target_family = "unix" => {
             mod unix;
             pub use unix::*;
-        };
+        }
         target_os = "lilium" => {
             mod lilium;
             pub use lilium::*;
-        };
+        }
+        target_family = "windows" => {
+            mod windows;
+            pub use windows::*;
+        }
     }
 
     #[cfg(target_arch = "x86_64")]
@@ -80,9 +109,22 @@ mod imp {
     mod x86;
 }
 
+/// A Generic Error type.
+/// This will contain more info later
 #[derive(Debug)]
 pub struct Error(());
 
+/// Determines the complete name of the system.
+///
+///
+/// # Implementation
+///
+/// This function is implemented in various ways depending on the host OS.
+/// On unix-like systems, it calls the `uname(2)` function for most fields.
+/// On Lilium, this uses `GetSystemInfo` with the `SysInfoRequestKernelVendor`, `SysInfoRequestOsVersion`, `SysInfoRequestArchInfo`, and `SysInfoRequestProcessorName` to gather information.
+///
+/// On Windows this uses a number of system calls.
+///
 pub fn uname() -> Result<Uname, Error> {
     let mut uname = Uname::new();
     imp::populate_uname(&mut uname).map_err(|_| Error(()))?;
